b39db1530d035872542b05343415e32bdf004394
DylanS97 <67909829+DylanS97@users.noreply.github.com>
2021-09-15 11:00:49 +0100

    Moved the details tab and products tab for they quotes create/edit into their own components.

---
+++ resources/js/views/components/DetailsTabComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="flex-1 rounded-md bg-gray-100 p-10 pl-0 pt-6">
+        <div class="p-2 mb-8 bg-gray-300 pl-10 w-92 relative border-t border-b border-gray-600 overflow-hidden">
+            <h2 class="text-2xl font-medium">Add Customer Details</h2>
+            <div class="title-tag bg-gray-100 h-10 w-10 top-1 left-88"></div>
+        </div>
+        <div class="flex justify-between bg-white rounded-md mb-4 ml-10">
+            <div class="p-4">
+                <div class="field-container w-80 h-14">
+                    <input class="field rounded-md" type="text" id="first_name" placeholder=" " v-model="details['client_first_name']">
+                    <label for="first_name" class="label bg-white top-4">First Name: <span class="text-red-500">*</span></label>
+                </div>
+                <span v-if="errors.client_first_name" class="text-red-500">{{ errors.client_first_name[0] }}</span>
+            </div>
+            <div class="p-4">
+                <div class="field-container w-80 h-14">
+                    <input class="field rounded-md" type="text" placeholder=" " id="last_name" v-model="details['client_last_name']">
+                    <label for="last_name" class="label bg-white top-4">Last Name: <span class="text-red-500">*</span></label>
+                </div>
+                <span v-if="errors.client_last_name" class="text-red-500">{{ errors.client_last_name[0] }}</span>
+            </div>
+        </div>
+        <div class="flex justify-between bg-white rounded-md ml-10">
+            <div class="p-4">
+                <div class="field-container w-80 h-14">
+                    <input class="field rounded-md" type="text" placeholder=" " id="phone" v-model="details['client_phone']">
+                    <label for="phone" class="label bg-white top-4">Contact No: <span class="text-red-500">*</span></label>
+                </div>
+                <span v-if="errors.client_phone" class="text-red-500">{{ errors.client_phone[0] }}</span>
+            </div>
+            <div class="p-4">
+                <div class="field-container w-80 h-14">
+                    <input class="field rounded-md" type="text" placeholder=" " id="email" v-model="details['client_email']">
+                    <label for="email" class="label bg-white top-4">Email: <span class="text-red-500">*</span></label>
+                </div>
+                <span v-if="errors.client_email" class="text-red-500">{{ errors.client_email[0] }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'detailsTab',
+
+    props: ['details', 'errors']
+}
+</script>
---
+++ resources/js/views/components/ProductsTabComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="flex-1 flex flex-col p-3 height-100% shadow-2xl bg-gray-100 rounded-md mx-w-800px">
+        <slot></slot>
+        <div class="flex-1 overflow-y-scroll">
+            <div v-if="product_items.length === 0" class="text-center p-6">
+                <span class="text-2xl text-gray-400">No products to display</span>
+            </div>
+            <div class="grid grid-cols-3">
+                <div class="product p-2 rounded-md m-2 shadow-lg bg-white" 
+                     v-for="(product, index) in product_items" :key="index">
+                    <div class="rounded-t-md overflow-hidden shadow-lg">
+                        <img v-if="product.images.length === 0"
+                            src="storage/images/ph.jpg" 
+                            alt="Placeholder">
+                        <img v-if="product.images.length > 0"
+                            :src="'storage/product_images/' + product.images[0].source" 
+                            :alt="product.images[0].alt">
+                    </div>
+                    <div class="flex justify-between py-4">
+                        <div>
+                            <span class="max-w-100 overflow-hidden product-name">{{ product.name | ucFirstLetter }}</span>
+                        </div>
+                        <div>
+                            <span class="text-right" style="height: min-content;">{{ product.price | priceFormat }}</span>
+                        </div>
+                    </div>
+
+                    <button @click="addToCart(product)" class="py-1 px-2 bg-green-500 rounded-md text-white text-sm float-right">Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'productTab',
+
+    props: [
+        'getProducts',
+        'product_items',
+        'addToCart'
+    ]
+}
+</script>
