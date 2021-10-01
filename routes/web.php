21ced0bc9c299163bd039fc19586d28a9073606f
DylanS97 <67909829+DylanS97@users.noreply.github.com>
2021-10-01 11:59:43 +0100

    Cleaned up the web routes and SCSS.

--- routes/web.php
+++ routes/web.php
@@ -23,25 +23,22 @@ Route::get('/login', function() {
     return view('auth.login');
 });
 
-Route::resource('quotes', QuoteController::class)->middleware(['auth']);
-
-Route::resource('products', ProductController::class)->middleware(['auth']);
-
-Route::resource('product_images', ProductImageController::class)->middleware(['auth']);
-
-Route::resource('cart', CartController::class)->middleware(['auth']);
-
-Route::resource('crumbs', CrumbsController::class)->middleware(['auth']);
-
-Route::get('{id}/images', [ProductImageController::class, 'index'])->middleware(['auth']);
-
-Route::post('{id}/images/create', [ProductImageController::class, 'store'])->middleware(['auth']);
-
-Route::get('mail-quote/{id}', [QuoteController::class, 'mailQuote'])->middleware(['auth']);
-
-Route::patch('/quote-completed/{id}', [QuoteController::class, 'updateCompleted'])->middleware(['auth']);
-
-Route::get('/{any}', [PagesController::class, 'index'])->middleware(['auth'])->where('any', '.*');
-
+Route::middleware(['auth'])->group(function() {
+    Route::resource('quotes', QuoteController::class)->middleware(['auth']);
+    Route::resource('products', ProductController::class)->middleware(['auth']);
+    Route::resource('product_images', ProductImageController::class)->middleware(['auth']);
+    Route::resource('cart', CartController::class)->middleware(['auth']);
+    Route::resource('crumbs', CrumbsController::class)->middleware(['auth']);
+    
+    Route::get('{id}/images', [ProductImageController::class, 'index'])->middleware(['auth']);
+    
+    Route::post('{id}/images/create', [ProductImageController::class, 'store'])->middleware(['auth']);
+    
+    Route::get('mail-quote/{id}', [QuoteController::class, 'mailQuote'])->middleware(['auth']);
+    
+    Route::patch('/quote-completed/{id}', [QuoteController::class, 'updateCompleted'])->middleware(['auth']);
+    
+    Route::get('/{any}', [PagesController::class, 'index'])->middleware(['auth'])->where('any', '.*');
+});
 
 require __DIR__.'/auth.php';
